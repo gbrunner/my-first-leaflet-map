2d2c0746f2ded350c9b0c90dc04c6ec56bf9adc0
Gregory Brunner <gbrunner@esri.com>
2021-06-10 12:47:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var mymap = L.map('mapid').setView([51.505, -0.09], 13);
 
-L.tileLayer('https://a.tile.stamen.com/toner/{z}/{x}/{y}.png', {
+L.tileLayer('https://a.tile.openstreetmap.org/{z}/{x}/{y}.png', {
         attribution: 'Map tiles by Stamen Design, under CC BY 3.0.',
         maxZoom: 18
 }).addTo(mymap);
